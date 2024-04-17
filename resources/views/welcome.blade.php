67cdc321aab9a62a2168bfa85af18a7d70b86094
sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-04-17 19:20:43 +0530

    edit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,7 +32,7 @@
             </div>
         </div>
     </nav>
-    <div class="container mx-auto p-2">
+    <div class="container">
         <h1>WELCOME TO BLOGS!!</h1>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
